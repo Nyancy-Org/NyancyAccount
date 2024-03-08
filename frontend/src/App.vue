2bc4ae38da528948b8e1857eabcef9c4b08429c0
Lazy <2211717435@qq.com>
2024-03-08 13:57:54 +0800

    新增 页脚

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <v-app>
+    <!-- <v-app-bar :elevation="1" title="NYANCY ACCOUNT">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+    </v-app-bar>
 
-  <RouterView />
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-main>
+      <v-container class="h-100">
+        <RouterView />
+      </v-container>
+    </v-main>
+
+    <Footer></Footer>
+  </v-app>
 </template>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer app>
+    <div class="text-center w-100 text-disabled">
+      &copy; Copyright {{ new Date().getFullYear() }} <strong class="text-primary">Lazy</strong> All
+      Right Reserved
+    </div>
+  </v-footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  background: transparent;
+  backdrop-filter: blur(10px);
+  strong {
+    opacity: 0.7;
+  }
+}
+</style>
