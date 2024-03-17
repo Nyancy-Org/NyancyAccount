0b4aec13f6aa53f786e0470181c65f10ca0aaaf6
Lazy <2211717435@qq.com>
2024-03-17 22:50:13 +0800

    优化 请求地址前缀

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -2,19 +2,22 @@ import { LoginForm, NyaResponse, RegForm, UserInfoRes } from '@/types'
 import axios from '@/utils/request'
 import { AxiosError } from 'axios'
 
+// 请求地址前缀
+axios.defaults.baseURL += '/auth'
+
 export type MailCodeType = 'reg' | 'changeEmail' | 'changePwd' | 'universal'
 export type MailLinkType = 'resetPwd'
 
 // 登录
 export const loginApi = async (formData: LoginForm) => {
-  const { data }: { data: UserInfoRes } = await axios.post('/auth/login?t_=' + Date.now(), formData)
+  const { data }: { data: UserInfoRes } = await axios.post('/login?t_=' + Date.now(), formData)
   return data
 }
 
 // 检查用户名
 export const checkUNameApi = async (formData: Pick<RegForm, 'username'>) => {
   const { data }: { data: NyaResponse } = await axios.post(
-    '/auth/checkUserName?t_=' + Date.now(),
+    '/checkUserName?t_=' + Date.now(),
     formData
   )
   return data
@@ -22,10 +25,7 @@ export const checkUNameApi = async (formData: Pick<RegForm, 'username'>) => {
 
 // 检查邮箱
 export const checkEmailApi = async (formData: Pick<RegForm, 'email'>) => {
-  const { data }: { data: NyaResponse } = await axios.post(
-    '/auth/checkEmail?t_=' + Date.now(),
-    formData
-  )
+  const { data }: { data: NyaResponse } = await axios.post('/checkEmail?t_=' + Date.now(), formData)
   return data
 }
 
@@ -34,7 +34,7 @@ export const sendEmailCodeApi = async (
   formData: Pick<RegForm, 'email'> & { type: MailCodeType }
 ) => {
   try {
-    const res = await axios.post('/auth/sendEmailCode?t_=' + Date.now(), formData)
+    const res = await axios.post('/sendEmailCode?t_=' + Date.now(), formData)
 
     const data: NyaResponse = res.data
     const retryAfter = res.headers['retry-after']
@@ -54,9 +54,6 @@ export const sendEmailCodeApi = async (
 
 // 注册
 export const regApi = async (formData: RegForm) => {
-  const { data }: { data: NyaResponse } = await axios.post(
-    '/auth/register?t_=' + Date.now(),
-    formData
-  )
+  const { data }: { data: NyaResponse } = await axios.post('/register?t_=' + Date.now(), formData)
   return data
 }
