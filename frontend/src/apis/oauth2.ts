3ac9a5102fc571338907bded4ea0759978f8fae0
Lazy <2211717435@qq.com>
2024-03-27 17:18:55 +0800

    新增 编辑 OAuth2 应用

--- frontend/src/apis/oauth2.ts
+++ frontend/src/apis/oauth2.ts
@@ -1,4 +1,4 @@
-import { NyaResponse, OAuth2ClientInfoRes } from '@/types'
+import { NyaResponse, OAuth2ClientInfo, OAuth2ClientInfoRes } from '@/types'
 import { axios } from '@/utils/request'
 
 // 请求地址前缀
@@ -10,6 +10,15 @@ const adminURL = baseURL + '/admin'
 export const getMyOAuth2AppsApi = async () => {
   const { data }: { data: OAuth2ClientInfoRes } = await axios.get(
     userURL + '/clients?t_=' + Date.now()
+  )
+  return data
+}
+
+// 更新我的 OAuth2 应用信息
+export const updateMyOAuth2AppApi = async (formData: OAuth2ClientInfo) => {
+  const { data }: { data: NyaResponse } = await axios.put(
+    userURL + '/client?t_=' + Date.now(),
+    formData
   )
   return data
 }
--- frontend/src/views/user/Apps.vue
+++ frontend/src/views/user/Apps.vue
@@ -3,8 +3,10 @@ import { ref, onMounted } from 'vue'
 import { OAuth2ClientInfo } from '@/types'
 import { getMyOAuth2AppsApi } from '@/apis/oauth2'
 import CopyTool from '@/components/CopyTool.vue'
+import oOauth2 from './dialog/oOauth2.vue'
 
 const serverItems = ref<OAuth2ClientInfo[]>([])
+const oOauth2Dialog = ref<InstanceType<typeof oOauth2>>()
 
 const getAppList = async () => {
   const { data } = await getMyOAuth2AppsApi()
@@ -17,61 +19,73 @@ onMounted(async () => {
 </script>
 
 <template>
-  <v-card-title class="d-flex align-center justify-space-between">
-    总计 {{ serverItems.length }} 个应用
-    <v-btn class="ml-4" variant="outlined" color="primary" prepend-icon="mdi-plus">添加</v-btn>
-  </v-card-title>
+  <div>
+    <v-card-title class="d-flex align-center justify-space-between">
+      总计 {{ serverItems.length }} 个应用
+      <v-btn
+        class="ml-4"
+        variant="outlined"
+        color="primary"
+        prepend-icon="mdi-plus"
+        @click="oOauth2Dialog?.openDialog()"
+        >添加</v-btn
+      >
+    </v-card-title>
 
-  <v-card-text>
-    <v-expansion-panels flat class="pa-0" variant="accordion">
-      <v-expansion-panel v-for="(item, i) in serverItems" :key="i" :title="item.name">
-        <v-expansion-panel-text>
-          <v-row align="center">
-            <v-col cols="12" sm="12" md="6">
-              <p class="text-subtitle-1 font-weight-medium">ID</p>
-              <span class="text-body-2 text-medium-emphasis">{{ item.id }}</span>
-            </v-col>
-            <v-col cols="12" sm="12" md="6">
-              <p class="text-subtitle-1 font-weight-medium">客户端 Secret</p>
-              <CopyTool :text="item.secret">
-                <template #default="{ copy, style, act }">
-                  <span
-                    v-bind="act"
-                    @click="copy"
-                    :style="style"
-                    class="text-body-2 text-medium-emphasis"
-                    >{{ item.secret }}</span
-                  >
-                </template>
-              </CopyTool>
-            </v-col>
-            <v-col cols="12" sm="12" md="6">
-              <p class="text-subtitle-1 font-weight-medium">回调 Url</p>
-              <CopyTool :text="item.redirect">
-                <template #default="{ copy, style, act }">
-                  <span
-                    v-bind="act"
-                    @click="copy"
-                    :style="style"
-                    class="text-body-2 text-medium-emphasis"
-                    >{{ item.redirect }}</span
-                  >
-                </template>
-              </CopyTool>
-            </v-col>
-            <v-col cols="12" sm="12" md="6">
-              <v-btn
-                color="primary"
-                class="mr-4"
-                variant="outlined"
-                prepend-icon="mdi-pencil-outline"
-                >编辑</v-btn
-              >
-              <v-btn color="red" variant="tonal" prepend-icon="mdi-delete-outline">删除</v-btn>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-text>
-      </v-expansion-panel>
-    </v-expansion-panels>
-  </v-card-text>
+    <v-card-text>
+      <v-expansion-panels flat class="pa-0" variant="accordion">
+        <v-expansion-panel v-for="(item, i) in serverItems" :key="i" :title="item.name">
+          <v-expansion-panel-text>
+            <v-row align="center">
+              <v-col cols="12" sm="12" md="6">
+                <p class="text-subtitle-1 font-weight-medium">ID</p>
+                <span class="text-body-2 text-medium-emphasis">{{ item.id }}</span>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <p class="text-subtitle-1 font-weight-medium">客户端 Secret</p>
+                <CopyTool :text="item.secret">
+                  <template #default="{ copy, style, act }">
+                    <span
+                      v-bind="act"
+                      @click="copy"
+                      :style="style"
+                      class="text-body-2 text-medium-emphasis"
+                      >{{ item.secret }}</span
+                    >
+                  </template>
+                </CopyTool>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <p class="text-subtitle-1 font-weight-medium">回调 Url</p>
+                <CopyTool :text="item.redirect">
+                  <template #default="{ copy, style, act }">
+                    <span
+                      v-bind="act"
+                      @click="copy"
+                      :style="style"
+                      class="text-body-2 text-medium-emphasis"
+                      >{{ item.redirect }}</span
+                    >
+                  </template>
+                </CopyTool>
+              </v-col>
+              <v-col cols="12" sm="12" md="6">
+                <v-btn
+                  color="primary"
+                  class="mr-4"
+                  variant="outlined"
+                  prepend-icon="mdi-pencil-outline"
+                  @click="oOauth2Dialog?.openDialog(item)"
+                  >编辑</v-btn
+                >
+                <v-btn color="red" variant="tonal" prepend-icon="mdi-delete-outline">删除</v-btn>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-text>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-card-text>
+
+    <oOauth2 ref="oOauth2Dialog" @update="getAppList" />
+  </div>
 </template>
