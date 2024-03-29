c5d9e21b9d7bcc16839db582018959bbb819442a
Lazy <2211717435@qq.com>
2024-03-29 21:13:31 +0800

    优化 标题栏

--- frontend/src/views/admin/index.vue
+++ frontend/src/views/admin/index.vue
@@ -42,10 +42,20 @@ const toLogout = async () => {
 </script>
 
 <template>
-  <v-app-bar :elevation="1" title="NYANCY ACCOUNT">
+  <v-app-bar :elevation="1" class="app-bar">
     <template #prepend>
       <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
     </template>
+    <v-app-bar-title class="app-bar-title">
+      <lazy-t>NYANCY ACCOUNT</lazy-t>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+
+    <img
+      title="Meow?"
+      class="img-meow"
+      src="https://s1.imlazy.ink:233/img/%E8%A1%A8%E6%83%85%E5%8C%85/%E7%8C%AB%E7%BE%BD%E9%9B%AB/015.png"
+    />
   </v-app-bar>
 
   <v-navigation-drawer :rail="rail" v-model="drawer">
@@ -72,3 +82,21 @@ const toLogout = async () => {
     </v-fade-transition>
   </RouterView>
 </template>
+
+<style lang="scss">
+.app-bar-title {
+  div {
+    overflow: unset !important;
+    lazy-t {
+      text-shadow: 0 0 10px #aaa;
+      transition: all 0.5s;
+      &:hover {
+        color: transparent;
+        background: linear-gradient(to right, #00ecff, #008fd4, #ff6afc);
+        background-clip: text;
+        -webkit-background-clip: text;
+      }
+    }
+  }
+}
+</style>
