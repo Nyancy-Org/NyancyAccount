726f0e58dc6a51511756567238e9cc46188ec36c
Lazy <2211717435@qq.com>
2024-04-22 20:33:39 +0800

    优化 分离管理员接口

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,21 +1,18 @@
 import {
   Controller,
-  HttpException,
-  HttpStatus,
   HttpCode,
   UseGuards,
   Session,
   Param,
   Put,
-  Query,
   Body,
   Get,
   Delete,
   Post,
 } from '@nestjs/common';
 import { UserService as UserServices } from './user.service';
-import { CheckAuthGuard, isAdmin } from 'src/Guard/permission';
-import type { UserInfo, UpdateType } from './user.interface';
+import { CheckAuthGuard } from 'src/Guard/permission';
+import type { UpdateType } from './user.interface';
 import type { RegistrationResponseJSON } from '@simplewebauthn/types';
 
 @Controller('user')
@@ -66,53 +63,5 @@ export class UserController {
     @Body() body: { credentialID: string },
   ) {
     return this.UserService.delete_wan(session, false, body);
-  }
-
-  /**
-   * 管理员接口
-   */
-  // 根据UID获取
-  @Get('info/:uid')
-  @UseGuards(isAdmin)
-  @HttpCode(200)
-  info_(@Param('uid') uid: string) {
-    return this.UserService.info_(uid);
-  }
-
-  // 用户列表
-  @Get('list')
-  @UseGuards(isAdmin)
-  @HttpCode(200)
-  list(
-    @Query('page') page = 1,
-    @Query('pageSize') pageSize = 10,
-    @Query('sortBy') sortBy: string,
-    @Query('sortDesc') sortDesc: string,
-    @Query('search') search: string,
-  ) {
-    if (!page || !pageSize)
-      throw new HttpException(
-        {
-          msg: '参数有误',
-        },
-        HttpStatus.EXPECTATION_FAILED,
-      );
-    return this.UserService.list(page, pageSize, sortBy, sortDesc, search);
-  }
-
-  // 更新指定用户信息
-  @Put('')
-  @UseGuards(isAdmin)
-  @HttpCode(200)
-  update_(@Body() body: UserInfo) {
-    return this.UserService.update_(body);
-  }
-
-  // 删除用户
-  @Delete('')
-  @UseGuards(isAdmin)
-  @HttpCode(200)
-  delete(@Body() body: UserInfo) {
-    return this.UserService.delete(body);
   }
 }
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserAdminController } from './user.admin.controller';
 import { AuthService } from 'src/auth/auth.service';
 
 @Module({
-  controllers: [UserController],
+  controllers: [UserController, UserAdminController],
   providers: [UserService, AuthService],
 })
 export class UserModule {}
